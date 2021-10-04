82419a6fbe185bb09f3f6f64fdf79f14f3b2c8a1
Ricardo Gumiero <rigumiero@gmail.com>
2021-10-04 15:27:32 -0300

    react-softphone implemented but gives an error

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { WebSocketInterface } from 'jssip'
+import SoftPhone from 'react-softphone';
 
 const CustomButton = ({text, color, action}) => {
   return (
@@ -14,6 +16,34 @@ const CallComponent = () => {
   const [isInCall, setIsInCall] = useState(false)
   const [mute, setMute] = useState(true)
 
+  const config = {
+    domain: '192.168.15.104', // sip-server@your-domain.io
+    uri: '10@192.168.15.104', // sip:sip-user@your-domain.io
+    password: 'xcom0615', //  PASSWORD ,
+    ws_servers: 'wss://10@192.168.15.104:8089/ws', //ws server
+    sockets: new WebSocketInterface('wss://192.168.15.104:8089/ws'),
+    display_name: 'Android',//jssip Display Name
+    debug: false // Turn debug messages on
+ 
+  };
+
+  const setConnectOnStartToLocalStorage = (newValue) => {
+  // Handle save the auto connect value to local storage
+  return true
+  }
+  const setNotifications = (newValue) => {
+  // Handle save the Show notifications of an incoming call to local storage
+  return true
+  }
+  const setCallVolume = (newValue) => {
+  // Handle save the call Volume value to local storage
+  return true
+  }
+  const setRingVolume = (newValue) => {
+  // Handle save the Ring Volume value to local storage
+  return true
+  }
+
   return (
     <View style={style.defaultView}>
       <CustomButton text={isInCall ? 'open' : 'answer'} color='limegreen' />
@@ -21,6 +51,18 @@ const CallComponent = () => {
       { isInCall && 
         <CustomButton action={() => setMute(!mute)} text={mute ? 'unmute' : 'mute'} color='royalblue' /> 
       }
+      <SoftPhone
+        callVolume={33} //Set Default callVolume
+        ringVolume={44} //Set Default ringVolume
+        connectOnStart={false} //Auto connect to sip
+        notifications={false} //Show Browser Notification of an incoming call
+        config={config} //Voip config
+        setConnectOnStartToLocalStorage={setConnectOnStartToLocalStorage} // Callback function
+        setNotifications={setNotifications} // Callback function
+        setCallVolume={setCallVolume} // Callback function
+        setRingVolume={setRingVolume} // Callback function
+        timelocale={'UTC-3'} //Set time local for call history
+      />
     </View>
   )
 
