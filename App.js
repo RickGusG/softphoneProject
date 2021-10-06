af572ed859b85a6589f8fd50c372f1335ff11f1f
Ricardo Gumiero <rigumiero@gmail.com>
2021-10-06 15:20:25 -0300

    successfully rejecting call

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Pressable, StyleSheet, Text, View } from 'react-native';
 import JsSIP from 'jssip'
 import {
   mediaDevices,
@@ -27,8 +27,11 @@ const CustomButton = ({text, color, action}) => {
 }
 
 const App = () => {
-  const [isInCall, setIsInCall] = useState(true)
-  const [mute, setMute] = useState(true)
+  const [isReceivingCall, setIsReceivingCall] = useState(false)
+  const [isInCall, setIsInCall] = useState(false)
+  const [session, setSession] = useState(null)
+  const [connected, setConnected] = useState(false)
+  const [registered, setReistered] = useState(false)
 
   const socket = new JsSIP.WebSocketInterface('ws://192.168.15.104:8088/asterisk/ws');
   const configuration = {
@@ -40,8 +43,17 @@ const App = () => {
 
   const ua = new JsSIP.UA(configuration);
 
-  ua.start()
-  ua.register()
+  const options = {
+    mediaConstraints: {
+      audio: true,
+      video: false
+    }
+  }
+
+  useEffect(() => {
+    ua.start()
+    if (connected) ua.register()
+  }, [])
 
   ua.on('connecting', () => {
     console.log("connecting");
@@ -49,14 +61,17 @@ const App = () => {
 
   ua.on('connected', () => {
     console.log("connected");
+    setConnected(true)
   });
 
-  ua.on('disconnected', (data) => {
+  ua.on('disconnected', () => {
     console.log('disconnected');
+    setConnected(false)
   });
 
   ua.on('unregistered', () => {
     console.log("client unregistered");
+    setReistered(false)
   });
 
   ua.on('progress', () => {
@@ -65,6 +80,7 @@ const App = () => {
 
   ua.on('registered', () => {             
     console.log("client registered");
+    setReistered(true)
   });
 
   ua.on('registrationFailed', (data) => {
@@ -72,50 +88,79 @@ const App = () => {
   })
 
   ua.on('newRTCSession', (data) => {
-    const {originator, session, request} = data;
-    console.log(originator, session, request)
+    const {session} = data;
+    
+    if (session.direction === 'incoming') {
+      console.log('receiving call')
+      setSession(session)
+      setIsReceivingCall(true)
+
+      session.on('connecting', () => console.log('connecting call'))
+
+      session.on('sending', () => console.log('sending call'))
+      
+      session.on('progress', () => console.log('call in progress'))
+
+      session.on('confirmed', () => console.log('call confirmed'))
+
+      session.on('accepted', () => console.log('call accepted'))
+
+      session.on('failed', (data) => {
+        console.log('call failed:', data)
+        setIsInCall(false)
+        setIsReceivingCall(false)
+        setSession(null)
+      })
+
+      session.on('ended', (data) => {
+        console.log('call ended:', data)
+        setIsInCall(false)
+        setIsReceivingCall(false)
+        setSession(null)
+      })
+    }
   })
 
-  const eventHandlers = {
-    'progress': () => {
-      console.log('call is in progress');
-    },
-    'failed': (e) => {
-      const {cause} = e
-      console.error('call failed with cause: '+ cause);
-    },
-    'ended': (e) => {
-      const {cause} = e
-      console.log('call ended with cause: '+ cause);
-    },
-    'confirmed': () => {
-      console.log('call confirmed');
-    }
-  };
+  const answerCall = () => {
+    console.log(session.answer)
+    console.log(session.answer(options))
+  }
 
-  const options = {
-    'eventHandlers'    : eventHandlers,
-    'mediaConstraints' : { 'audio': true, 'video': false }
-  };
+  const rejectOrHangupCall = () => {
+    session.terminate()
+    setSession(null)
+    setIsInCall(false)
+    setIsReceivingCall(false)
+  }
 
   return (
     <View style={style.defaultView}>
-      <CustomButton action={() => ua.call('20@192.168.15.104:5060', options)} text={isInCall ? 'open' : 'answer'} color='limegreen' />
-      <CustomButton text='reject' color='crimson' />
-      { isInCall && 
-        <CustomButton action={() => setMute(!mute)} text={mute ? 'unmute' : 'mute'} color='royalblue' /> 
-      }
+      <View style={{borderLeftWidth: 5, borderColor: registered ? 'royalblue' : connected ? 'limegreen' : 'crimson'}}>
+        <Text style={style.text}>{isReceivingCall ? 'someone is calling you' : 'proto app'}</Text>
+      </View>
+      {isReceivingCall && (<View style={style.contentView}>
+        <CustomButton action={answerCall} text={isInCall ? 'open' : 'answer'} color='limegreen' />
+        <CustomButton action={rejectOrHangupCall} text='reject' color='crimson' />
+        { isInCall && <CustomButton text={'unmute'} color='royalblue' /> }
+      </View>)}
     </View>
   )
 };
 
+const {width, height} = Dimensions.get('window')
 const style = StyleSheet.create({
+  contentView: {
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    width,
+  },
   defaultView: {
     alignItems: 'center',
     justifyContent: 'space-evenly',
     flex: 1,
     backgroundColor: '#fff',
-    flexDirection: 'row'
   },
   btn: {
     height: 80,
@@ -127,6 +172,12 @@ const style = StyleSheet.create({
   btnTxt: {
     fontWeight: 'bold',
     fontSize: 25
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 35,
+    fontFamily: 'sans-serif-condensed',
+    color: 'black'
   }
 })
 
